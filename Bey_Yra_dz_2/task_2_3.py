51642973e80200843a45b488d1146d8a32b9f0af
4erons <104031725+4erons@users.noreply.github.com>
2022-06-05 23:03:46 +0300

    Revert "lessons02"
    This reverts commit e9de2caf6cf251d4117b6de96ec48a7b7acd64e8.

--- Bey_Yra_dz_2/task_2_3.py
+++
@@ -1,6 +0,0 @@
-list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-answer = {}
-
-for elem in range(len(list)):
-    normal_view = list[elem].split(' ')[len(list[elem].split(' ')) -1]
-    print(f"Привет, {normal_view.lower().title()}!")
